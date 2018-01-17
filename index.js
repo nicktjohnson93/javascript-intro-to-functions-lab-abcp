c96135d53b7c1023e4ef8011fb42a89e8c57ae04
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-17 02:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   else if (string === "HELLO"){
     return "YES INDEED!"
   }
-  else if (string === "I love you")
+  else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
