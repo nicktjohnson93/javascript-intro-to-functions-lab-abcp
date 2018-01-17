79cc2ed1c0fbff044cdda9bdefd5104c70ca93da
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-17 01:54:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+logShout("hello")
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+logWhisper("hello")
+
+function sayHiToGrandma(string){
+  if 
+  
+}
